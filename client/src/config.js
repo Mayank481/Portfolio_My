b9b1823b39d1cd829a24e80fbf474ad62d41212e
Mayank481 <jaggi.mayank19@gmail.com>
2024-11-10 17:12:25 +0530

    add profile's link

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,4 @@
-export let API_URL = "http://localhost:5000/api/v1";
+// export let API_URL = "http://localhost:5000/api/v1";
+export let API_URL = "https://portfolio-pprt.onrender.com/api/v1";
 
 export const API_CONTACT = `${API_URL}/user/sendEmail`;
